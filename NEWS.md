0597c00d53212762a158bdb8dedb0786d07980ff
dennis feehan <feehan@berkeley.edu>
2019-03-11 12:22:41 -0700

    still moving code into package

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# siblingsurvival 0.1.0
+
+* Added a `NEWS.md` file to track changes to the package.
---
+++ R/siblingsurvival.R
@@ -0,0 +1,3 @@
+##' @useDynLib siblingsurvival, .registration = TRUE
+##' @importFrom Rcpp sourceCpp
+NULL
