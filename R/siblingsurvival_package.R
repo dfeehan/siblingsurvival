5b83f7ade80a665ecae56887e5b5f54dd702426f
dennis feehan <feehan@berkeley.edu>
2020-06-20 14:48:54 -0700

    re-ran use_rcpp

--- R/siblingsurvival_package.R
+++ R/siblingsurvival_package.R
@@ -1,8 +1,8 @@
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
 #' @import tidyverse
 #' @import dplyr
 #' @import magrittr
 #' @import tidyr
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
