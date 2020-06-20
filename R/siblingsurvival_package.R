7f291aa50895c15783b7f16711281b9e77f66d3f
dennis feehan <feehan@berkeley.edu>
2020-06-20 14:33:18 -0700

    trying to fix namespace issues...

--- R/siblingsurvival_package.R
+++ R/siblingsurvival_package.R
@@ -1,6 +1,8 @@
-##' @useDynLib siblingsurvival, .registration = TRUE
-##' @importFrom Rcpp sourceCpp
-##' @import tidyverse
-##' @import dplyr
-##' @import magrittr
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @import tidyverse
+#' @import dplyr
+#' @import magrittr
+#' @import tidyr
+## usethis namespace: end
 NULL
