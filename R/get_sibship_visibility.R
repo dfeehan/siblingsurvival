793e1bd20ae8c0106dcf2d4f9625ea89bfcc3b88
dennis feehan <feehan@berkeley.edu>
2025-01-30 15:34:26 -0800

    first draft adding maternal functions

--- R/get_sibship_visibility.R
+++ R/get_sibship_visibility.R
@@ -1,6 +1,6 @@
 ##' calculate visibility for each sibship
 ##'
-##' @param sib.dat The long-form sibling dataset (likely produced by TODO)
+##' @param sib.dat The long-form sibling dataset (likely produced by [siblingsurvival::prep_dhs_sib_histories])
 ##' @param ego.id  String with the name of the column in \code{sib.dat} containing the survey respondent ID
 ##' @param sib.frame.indicator String with the name of the column in \code{sib.dat} containing a 0/1 coded variable indicating whether or not each sib is in the frame population
 ##' @return A tibble with a row for each survey respondent (each unique value of \code{ego.id}), and the number of sibs the respondent reported on the frame, including and not including herself
@@ -23,3 +23,135 @@ get_sibship_visibility <- function(sib.dat,
 
   return(vis.dat)
 }
+
+##' calculate visibility for each sibship and ego
+##'
+##' @param ego.dat The ego dataset (likely produced by [siblingsurvival::prep_dhs_sib_histories])
+##' @param ego.id  String with the name of the column in \code{sib.dat} containing the survey respondent ID
+##' @param sib.dat The long-form sibling dataset (likely produced by [siblingsurvival::prep_dhs_sib_histories])
+##' @param sib.frame.indicator String with the name of the column in \code{sib.dat} containing a 0/1 coded variable indicating whether or not each sib is in the frame population
+##' @return A list with two entries:
+##'   * `ego_vis_agg` - a tibble with summarized adjustment factors
+##'   * `ego_vis` - a tibble with one row per ego and the ego-specific visibilities
+##'   tibble with a row for each survey respondent (each unique value of \code{ego.id}),
+##'   and the number of sibs the respondent reported on the frame, including and not including herself
+##' @examples
+##'   # TODO write example code
+get_visibility <- function(ego.dat,
+                           ego.id,
+                           sib.dat,
+                           sib.frame.indicator) {
+
+  sib.dat <- sib.dat %>%
+    # rename the ego id and sibling frame indicator variables
+    # to make the code more flexible
+    rename(.ego.id = !!sym(ego.id),
+           .sib.in.F = !!sym(sib.frame.indicator))
+
+  ego.dat <- ego.dat %>%
+    rename(.ego.id=!!sym(ego.id))
+
+  ###################################
+  ## calculate quantities related to visibility for each respondent's sibship
+
+  # for each sibship, calculate the number of reported sibs on the frame
+  # and the size of the sibship
+  sib_F_dat <- sib.dat %>%
+    group_by(.ego.id) %>%
+    summarize(# y.F is the number of siblings in the frame population
+              # reported by the respondent
+              # (note that dead sibs are never in the frame popn)
+              y.F = sum(.sib.in.F),
+              # total size of sibship, which is number of reported
+              # siblings plus one (for the respondent)
+              sib.size = n() + 1)
+
+  # add the sibling size to the ego data
+  ego_vis <- ego.dat %>%
+    select(.ego.id, wwgt, age.cat, sex) %>%
+    left_join(sib_F_dat, by='.ego.id')
+
+  # if nothing was joined in, there are no sibs
+  ego_vis <- ego_vis %>%
+    mutate(y.F=ifelse(is.na(y.F), 0, y.F),
+           y.Fplusone = y.F + 1,
+           sib.size = ifelse(is.na(sib.size), 1, sib.size))
+
+  ###################################
+  ## calculate summaries + adjustment factors based on the
+  ## ego-specific visibilities
+
+  # weighted harmonic mean
+  wh.mean <- function(x, w) {
+    return(sum(w) / sum(w/x))
+  }
+
+  # TODO comment
+  S.hat <- wh.mean(ego_vis$y.Fplusone, ego_vis$wwgt)
+  S.adj.factor <- 1 - (1/S.hat)
+
+  # TODO comment
+  y.F.bar <- weighted.mean(ego_vis$y.F, ego_vis$wwgt)
+  approx.S.hat <- y.F.bar + 1
+  approx.S.adj.factor <- 1 - (1/approx.S.hat)
+
+  # TODO comment
+  ego_vis_agg <- ego_vis %>%
+    mutate(age.cat = paste(age.cat)) %>%
+    group_by(sex, age.cat) %>%
+
+    summarise(# this is the average y.F value across egos
+              y.F.bar = weighted.mean(y.F, wwgt),
+              # this is the average sibship size (which will be
+              # size-biased)
+              avg.sib.size = weighted.mean(sib.size, wwgt)) %>%
+
+    mutate(adj.factor = S.adj.factor,
+           # this is the all-ages approximation
+           adj.factor.allage = approx.S.adj.factor,
+           # this is the age-specific approximation
+           adj.factor.agespec = y.F.bar / (y.F.bar + 1))
+
+  return(lst(ego_vis,
+             ego_vis_agg))
+
+}
+
+##' get_ego_age_distribution
+##'
+##' @param ego_dat the ego dataset (probably from [siblingsurvival::prep_dhs_sib_histories])
+##' @param only_females should only females be used to calculate age distribution? (default: True)
+##'
+##' @return dataframe with distribution of respondent ages by 5-year category,
+##' typically used in calculating the maternal or pregnancy-related mortality
+##' @section Details:
+##' `ego_dat` is assumed to have two columns: `wwgt` and `age.cat`
+get_ego_age_distn <- function(ego.dat,
+                              only_females = TRUE) {
+
+  if(only_females) {
+    ego.dat <- ego.dat %>% filter(sex == 'f')
+  }
+
+  respondent_age <- ego.dat %>%
+    ## age.cat and wwgt are assumed to come with the dataset; they have
+    ## ego age in 5-year groups and the women's weight
+    filter(age.cat %in% c("[15,20)",
+                          "[20,25)",
+                          "[25,30)",
+                          "[30,35)",
+                          "[35,40)",
+                          "[40,45)",
+                          "[45,50)")) %>%
+    group_by(age.cat) %>%
+    # note that [siblingsurvival::prep_dhs_sib_histories]
+    # will have already scaled these weights
+    summarize(total = sum(wwgt))
+
+  respondent_age$agegrp_prop <- respondent_age$total / (sum(respondent_age$total))
+
+  return(respondent_age)
+}
+
+
+
---
+++ R/maternal_estimators.R
@@ -0,0 +1,125 @@
+##' calculate total rate based on point estimates
+##'
+##' @param estimates the output of [siblingsurvival::sibling_estimator]
+##' @param ego.dat the prepped ego data
+##' @param sib.dat the prepped sibling data
+##'
+##' @returns either a tibble with the estimates aggregated across age groups
+##' OR, if there are bootstrap results, then a list with three entries:
+##'   * `point` - the point estimates (should be the same as running w/out bootstraps)
+##'   * `boot_summ` - estimates w/ confidence intervals calculated from bootstrap reps
+##'   * `boot` - results for each bootstrap rep
+##'
+##' @section Details:
+##'
+##' This function aggregates age-specific maternal mortality quantities
+##' like the maternal death rate or the pregnancy-related death rate.
+##' It uses the age distribution of the survey respondents as the reference
+##' population.
+##'
+##' Note that this will only consider estimates for females aged 15-49,
+##' as this is what makes sense for aggregating maternal estimates
+##'
+aggregate_maternal_estimates <- function(estimates,
+                                         ego.dat,
+                                         sib.dat) {
+
+
+  # get age distribution of respondents
+  age_prop <- get_ego_age_distn(ego.dat)
+  vis_res <- get_visibility(ex.ego,
+                            ego.id='caseid',
+                            ex.sib,
+                            sib.frame.indicator='in.F')
+
+  res <- estimates$asdr.ind %>%
+    select(time.period, sib.sex, sib.age,
+           # if we specified 'event.name' in the cell.config, keep it...
+           any_of('event.name'),
+           asdr.hat.ind = asdr.hat) %>%
+    left_join(estimates$asdr.agg %>%
+                select(time.period, sib.sex, sib.age,
+                       any_of('event.name'),
+                       asdr.hat.agg = asdr.hat)) %>%
+    # only need ages 15-50
+    filter(! sib.age %in% c("[50,55)", "[55,60)", "[60,65)")) %>%
+    # only need females
+    filter(sib.sex  == 'f') %>%
+    left_join(age_prop, by=c('sib.age'='age.cat')) %>%
+    left_join(vis_res$ego_vis_agg, by=c('sib.age'='age.cat')) %>%
+    mutate(dummy=1) %>%
+    group_by(dummy) %>%
+    summarize(ind.est = sum(asdr.hat.ind*agegrp_prop),
+              agg.est = sum(asdr.hat.agg*agegrp_prop),
+              adj.factor = adj.factor[1],
+              adj.factor.allage = adj.factor.allage[1],
+              adj.factor.meanagespec = sum(adj.factor.agespec*agegrp_prop)) %>%
+    mutate(ratio.agg.ind = agg.est / ind.est) %>%
+    mutate(ratio.ind.agg = ind.est  / agg.est) %>%
+    select(-dummy)
+
+  ## if there are bootstrap results, also calculate aggregate for those
+  if ('boot.asdr.ind' %in% names(estimates)) {
+
+    # calculate confidence intervals for the total estimates
+    # (the ones across all age groups), based on the bootstrap rep
+    calculate_ci_for_total <- function(total_boot_res) {
+
+      res <- total_boot_res %>%
+        mutate(dummy=1) %>%
+        group_by(dummy) %>%
+        summarize_at(vars(ind.est, agg.est,
+                          adj.factor, adj.factor.allage, adj.factor.meanagespec,
+                          ratio.agg.ind,
+                          ratio.ind.agg),
+                     list( .ci.low = ~ quantile(.x, .025, na.rm=TRUE),
+                           .ci.high = ~ quantile(.x, .975, na.rm=TRUE),
+                           .mean = ~ mean(.x, na.rm=TRUE))) %>%
+        select(-dummy) %>%
+        rename_with(~ str_replace(.x, "_", ""))
+
+      return(res)
+
+    }
+
+    res_boot <- estimates$boot.asdr.ind %>%
+      select(time.period, sib.sex, sib.age,
+             boot_idx,
+             # if we specified 'event.name' in the cell.config, keep it...
+             any_of('event.name'),
+             asdr.hat.ind = asdr.hat) %>%
+      left_join(estimates$boot.asdr.agg %>%
+                  select(time.period, sib.sex, sib.age,
+                         boot_idx,
+                         any_of('event.name'),
+                         asdr.hat.agg = asdr.hat)) %>%
+      # only need ages 15-50
+      filter(! sib.age %in% c("[50,55)", "[55,60)", "[60,65)")) %>%
+      # only need females
+      filter(sib.sex  == 'f') %>%
+      left_join(age_prop, by=c('sib.age'='age.cat')) %>%
+      left_join(vis_res$ego_vis_agg, by=c('sib.age'='age.cat')) %>%
+      group_by(boot_idx) %>%
+      summarize(ind.est = sum(asdr.hat.ind*agegrp_prop),
+                agg.est = sum(asdr.hat.agg*agegrp_prop),
+                adj.factor = adj.factor[1],
+                adj.factor.allage = adj.factor.allage[1],
+                adj.factor.meanagespec = sum(adj.factor.agespec*agegrp_prop)) %>%
+      mutate(ratio.agg.ind = agg.est / ind.est) %>%
+      mutate(ratio.ind.agg = ind.est  / agg.est)
+
+    res_boot_summ <- calculate_ci_for_total(res_boot) %>%
+      select(order(colnames(.)))
+
+    res <- list(point = res,
+                boot_summ = res_boot_summ,
+                boot = res_boot)
+  }
+
+
+  return(res)
+
+
+}
+
+
